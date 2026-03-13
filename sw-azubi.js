7077665e2e3e66520f761fd4cb9044172b755d47
Mohr-App <stefan.mohr78@googlemail.com>
2026-03-13 10:36:41 +0100

    Add files via upload

--- sw-azubi.js
+++ sw-azubi.js
@@ -1,4 +1,4 @@
-const CACHE = 'azubipro-v16';
+const CACHE = 'azubipro-v17';
 const ASSETS = [
   './AzubiPro.html',
   './manifest-azubi.json',
--- sw-vp.js
+++ sw-vp.js
@@ -1,4 +1,4 @@
-const CACHE = 'verwaltungpro-v28';
+const CACHE = 'verwaltungpro-v29';
 const ASSETS = [
   './HBZVerwaltung.html',
   './manifest-vp.json',
