eafb7e87b250054cc25b204ba6395962e4a8edf6
Mohr-App <stefan.mohr78@googlemail.com>
2026-03-28 13:47:02 +0100

    Add files via upload

--- sw-vp.js
+++ sw-vp.js
@@ -1,4 +1,4 @@
-const CACHE = 'verwaltungpro-v116';
+const CACHE = 'verwaltungpro-v117';
 const ASSETS = [
   './HBZVerwaltung.html',
   './manifest-vp.json',
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'ausbilderpro-v182';
+const CACHE = 'ausbilderpro-v183';
 const ASSETS = [
   './AusbilderPro.html',
   './manifest.json',
