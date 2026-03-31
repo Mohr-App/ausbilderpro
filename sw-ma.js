cc490f01e22ce0a004f8260f85bbed971a8c9906
AusbilderPro Deploy <deploy@ausbilderpro.de>
2026-03-31 12:50:52 +0000

    MitarbeiterPro App + AG kann Arbeitnehmer anlegen + Typ-Badges + Landingpage 5 Apps

---
+++ sw-ma.js
@@ -0,0 +1,5 @@
+const V='mitarbeiterpro-v1';
+const A=['/ausbilderpro/MitarbeiterPro.html'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(V).then(c=>c.addAll(A)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==V).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(V).then(ca=>ca.put(e.request,c));return r;}).catch(()=>caches.match(e.request)));});
