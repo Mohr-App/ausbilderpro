8fc6e533576a48afe767a566f631c25505ab98ef
Stefan Mohr <stefan@ausbilderpro.de>
2026-04-01 08:36:15 +0000

    Alle Apps: Handy-Zurück-Button navigiert innerhalb der App statt zu schließen

--- sw-azubi.js
+++ sw-azubi.js
@@ -1,4 +1,4 @@
-const CACHE = 'azubipro-v76';
+const CACHE = 'azubipro-v77';
 const ASSETS = [
   './AzubiPro.html',
   './manifest-azubi.json',
--- sw-vp.js
+++ sw-vp.js
@@ -1,4 +1,4 @@
-const CACHE = 'verwaltungpro-v127';
+const CACHE = 'verwaltungpro-v128';
 const ASSETS = [
   './HBZVerwaltung.html',
   './manifest-vp.json',
