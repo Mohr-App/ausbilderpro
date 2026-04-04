82354466ad0aea784f3aaaf90733c43612d79360
Stefan Mohr <stefan@ausbilderpro.de>
2026-04-04 10:41:57 +0000

    Hilfe/Anleitung aktualisiert in allen 5 Apps – Zeiterfassung, Aufträge, Geofencing, Gleitzeit, DATEV

--- sw-azubi.js
+++ sw-azubi.js
@@ -1,4 +1,4 @@
-const CACHE = 'azubipro-v93';
+const CACHE = 'azubipro-v94';
 const ASSETS = [
   './AzubiPro.html',
   './manifest-azubi.json',
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'ausbilderpro-v222';
+const CACHE = 'ausbilderpro-v223';
 const ASSETS = [
   './AusbilderPro.html',
   './manifest.json',
