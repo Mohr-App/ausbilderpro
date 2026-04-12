038243abb1e4449231d4cca1c019d61c988aace5
Stefan Mohr <stefan@ausbilderpro.de>
2026-04-12 20:55:46 +0000

    Share: ausbilderpro.de URLs, nur Kopieren+Teilen (kein WA/Mail extra), AG Modal vereinfacht

--- sw-azubi.js
+++ sw-azubi.js
@@ -1,4 +1,4 @@
-const CACHE = 'azubipro-v101';
+const CACHE = 'azubipro-v102';
 const ASSETS = [
   './AzubiPro.html',
   './manifest-azubi.json',
--- sw-ma.js
+++ sw-ma.js
@@ -1,4 +1,4 @@
-const V='mitarbeiterpro-v34';
+const V='mitarbeiterpro-v35';
 const A=['/ausbilderpro/MitarbeiterPro.html'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(V).then(c=>c.addAll(A)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==V).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
