d9273ae15dc3ecf1e9be7a5b6a9ea0087f1f568d
Deploy <deploy@ausbilderpro.de>
2026-04-15 10:39:53 +0000

    Tooltips: 202 title-Attribute auf Buttons in allen 5 Apps (Hover-Info)

--- sw-azubi.js
+++ sw-azubi.js
@@ -1,4 +1,4 @@
-const CACHE = 'azubipro-v107';
+const CACHE = 'azubipro-v108';
 const ASSETS = [
   './AzubiPro.html',
   './manifest-azubi.json',
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'ausbilderpro-v231';
+const CACHE = 'ausbilderpro-v232';
 const ASSETS = [
   './AusbilderPro.html',
   './manifest.json',
