5c7033ec77dbbdf1ce3586648d970c89d4a5a49f
Stefan Mohr <stefan.mohr78@googlemail.com>
2026-04-21 16:12:53 +0000

    AG v132 + VP v166 + AzubiPro v144 + MA v78 + AusbilderPro v276: Launcher-Display-Fix
    Bug 1: Auf Desktop war der Launcher nach Login nicht sichtbar.
    Ursache: style.display='' fuehrt zu Inherit vom Default des Elements.
    Da .app-launcher-wrap die CSS-Regel display:none hat, blieb es hidden.
    
    Fix: Inline-Style auf 'inline-block' setzen. Ueberschreibt die
    CSS-Regel korrekt.
    
    Alle 5 Apps synchron gefixt:
    - ArbeitgeberPro
    - AusbilderPro
    - AzubiPro
    - MitarbeiterPro
    - VerwaltungPro
    
    Nach Hard-Reload sollte der 3x3-Punkte-Launcher rechts oben im Header
    auf Desktop sichtbar sein fuer Admin/Verwaltung-Rollen.

--- sw-azubi.js
+++ sw-azubi.js
@@ -1,4 +1,4 @@
-const CACHE = 'azubipro-v143';
+const CACHE = 'azubipro-v144';
 const ASSETS = [
   './AzubiPro.html',
   './manifest-azubi.json',
--- sw-vp.js
+++ sw-vp.js
@@ -1,4 +1,4 @@
-const CACHE = 'verwaltungpro-v165';
+const CACHE = 'verwaltungpro-v166';
 const ASSETS = [
   './HBZVerwaltung.html',
   './manifest-vp.json',
