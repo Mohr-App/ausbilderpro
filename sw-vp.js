9c1e177c5a45e33ff77560e757b09a846f99c47c
Stefan Mohr <stefan.mohr@claude>
2026-05-15 10:36:20 +0000

    AP v456 + VP v247: Trennlinien-Symbol Diamond -> Punkt

--- sw-vp.js
+++ sw-vp.js
@@ -1,4 +1,4 @@
-const CACHE = 'verwaltungpro-v246';
+const CACHE = 'verwaltungpro-v247';
 const ASSETS = [
   './HBZVerwaltung.html',
   './manifest-vp.json',
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'ausbilderpro-v455';
+const CACHE = 'ausbilderpro-v456';
 const ASSETS = [
   './AusbilderPro.html',
   './manifest.json',
