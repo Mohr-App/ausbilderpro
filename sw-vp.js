49f67c04910440be510d24230bf3dadffcde7687
Stefan Mohr <s.mohr@ish.de>
2026-08-18 13:49:45 +0000

    VP Build 359: Dashboard Laeuft-jetzt zeigt aktiv/zugewiesen + bestaetigt. AusbilderPro v830: Kurs-Check ab 2. Kurstag (TN ohne Einordnung)

--- sw-vp.js
+++ sw-vp.js
@@ -1,4 +1,4 @@
-const CACHE = 'verwaltungpro-v358';
+const CACHE = 'verwaltungpro-v359';
 const ASSETS = [
   './HBZVerwaltung.html',
   './manifest-vp.json',
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'ausbilderpro-v829';
+const CACHE = 'ausbilderpro-v830';
 const ASSETS = [
   './AusbilderPro.html',
   './manifest.json',
