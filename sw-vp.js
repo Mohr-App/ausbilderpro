8c081cd4f30a55a774a9ca871652243b5fc7bb92
Mohr-App <info@ausbilderpro.de>
2026-08-21 06:01:59 +0000

    QR-Codes lokal erzeugen (assets/qrcode.js, _qrDataUrl mit qrserver-Fallback): 4 Stellen AusbilderPro, 1 Stelle HBZVerwaltung | v836 / Build 364

--- sw-vp.js
+++ sw-vp.js
@@ -1,4 +1,4 @@
-const CACHE = 'verwaltungpro-v363';
+const CACHE = 'verwaltungpro-v364';
 const ASSETS = [
   './HBZVerwaltung.html',
   './manifest-vp.json',
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'ausbilderpro-v835';
+const CACHE = 'ausbilderpro-v836';
 const ASSETS = [
   './AusbilderPro.html',
   './manifest.json',
