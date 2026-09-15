2b6448cf6424b10bbbf256ebc762e981cceece63
Stefan Mohr <s.mohr@ish.de>
2026-09-15 07:35:37 +0000

    VP Build 415 + AzubiPro v326: Platznummer bei Kurszuweisung geleert; Platzabfrage nach QR-Check-in solange unbestaetigt

--- sw-azubi.js
+++ sw-azubi.js
@@ -1,4 +1,4 @@
-const CACHE = 'azubipro-v325';
+const CACHE = 'azubipro-v326';
 const ASSETS = [
   './AzubiPro.html',
   './manifest-azubi.json',
--- sw-vp.js
+++ sw-vp.js
@@ -1,4 +1,4 @@
-const CACHE = 'verwaltungpro-v414';
+const CACHE = 'verwaltungpro-v415';
 const ASSETS = [
   './HBZVerwaltung.html',
   './manifest-vp.json',
