05e33fe0f7b104df607805bc64f163becb0d8a73
Stefan Mohr <s.mohr@ish.de>
2026-09-15 13:03:03 +0000

    AusbilderPro v955 + VP Build 420: Bemerkung fuer Anwesenheitsliste bleibt einzeilig im Druck (kleinere Schrift statt Umbruch), 75 Zeichen erlaubt

--- sw-vp.js
+++ sw-vp.js
@@ -1,4 +1,4 @@
-const CACHE = 'verwaltungpro-v419';
+const CACHE = 'verwaltungpro-v420';
 const ASSETS = [
   './HBZVerwaltung.html',
   './manifest-vp.json',
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'ausbilderpro-v954';
+const CACHE = 'ausbilderpro-v955';
 const ASSETS = [
   './AusbilderPro.html',
   './manifest.json',
