9ffc6f04861a12d10af4335d4cfccb409c6a5b7b
Stefan Mohr <s.mohr@ish.de>
2026-09-18 06:31:21 +0000

    AusbilderPro v968 + VP Build 425: rotes X statt U fuer unentschuldigt (U ist am HBZ fuer Urlaub belegt), Legenden angepasst

--- sw-vp.js
+++ sw-vp.js
@@ -1,4 +1,4 @@
-const CACHE = 'verwaltungpro-v424';
+const CACHE = 'verwaltungpro-v425';
 const ASSETS = [
   './HBZVerwaltung.html',
   './manifest-vp.json',
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'ausbilderpro-v967';
+const CACHE = 'ausbilderpro-v968';
 const ASSETS = [
   './AusbilderPro.html',
   './manifest.json',
